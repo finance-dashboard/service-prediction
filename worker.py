dedc094f304203ffdb040530f6557f9f24f9de56
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2021-12-22 21:19:05 +0500

    Get Redis address from environment, reconnect on failure

--- worker.py
+++ worker.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
+import os
 import sys
+import logging
+import time
 
 # Preimport required modules
 import numpy as np
 from statsmodels.tsa.arima.model import ARIMA
+from redis import Redis
 from rq import Connection, Worker
 
-with Connection():
-    qs = sys.argv[1:] or 'default'
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
-    w = Worker(qs)
-    w.work()
+REDIS_CONN = Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+TRY_COUNTER = 10
+
+while TRY_COUNTER > 0:
+    try:
+        with Connection(REDIS_CONN):
+            qs = sys.argv[1:] or 'default'
+
+            w = Worker(qs)
+            w.work()
+    # TODO: handle specific connection error exception
+    except:
+        logging.warning(f'Unable to connect. {TRY_COUNTER} attempts left')
+        time.sleep(5)
+        TRY_COUNTER -= 1
